81825950541135598b77e2cc016dc504aeebee2d
Josh Zwiebel <joshzwiebel@gmail.com>
2023-03-06 16:56:13 -0500

    maybe first draft?

--- Assignment-2/transhipment.py
+++ Assignment-2/transhipment.py
@@ -2,4 +2,63 @@ import cvxpy as cp
 import numpy as np
 
 
-print((20772297 % 199) % 3)
+edge_AD = cp.Variable(1, integer=True)
+edge_AB = cp.Variable(1, integer=True)
+edge_AC = cp.Variable(1, integer=True)
+edge_DB = cp.Variable(1, integer=True)
+edge_BC = cp.Variable(1, integer=True)
+edge_BE = cp.Variable(1, integer=True)
+edge_CE = cp.Variable(1, integer=True)
+
+constraints = [edge_AD + edge_AB + edge_AC == -2,
+               edge_DB - edge_AB == 7,
+               - edge_DB - edge_AB + edge_BC == -2,
+               -edge_AC - edge_BC + edge_CE == 3,
+               -edge_BE - edge_CE == -6]
+
+maximize = cp.Minimize(3 * cp.abs(edge_AD) + 6 * cp.abs(edge_AB) + 4 * cp.abs(edge_AC)
+                       + 5 * cp.abs(edge_DB) + 2 * cp.abs(edge_BC) + 7 * cp.abs(edge_BE))
+
+prob = cp.Problem(maximize, constraints)
+
+prob.solve()
+
+print("The optimal value is", prob.value)
+print("The value of edge_AD is", edge_AD.value)
+print("The value of edge_AB is", edge_AB.value)
+print("The value of edge_AC is", edge_AC.value)
+print("The value of edge_DB is", edge_DB.value)
+print("The value of edge_BC is", edge_BC.value)
+print("The value of edge_BE is", edge_BE.value)
+print("The value of edge_CE is", edge_CE.value)
+
+
+edge_AD = cp.Variable(1, )
+edge_AB = cp.Variable(1, )
+edge_AC = cp.Variable(1, )
+edge_DB = cp.Variable(1, )
+edge_BC = cp.Variable(1, )
+edge_BE = cp.Variable(1, )
+edge_CE = cp.Variable(1, )
+
+constraints = [edge_AD + edge_AB + edge_AC == -2,
+               edge_DB - edge_AB == 7,
+               - edge_DB - edge_AB + edge_BC == -2,
+               -edge_AC - edge_BC + edge_CE == 3,
+               -edge_BE - edge_CE == -6]
+
+maximize = cp.Minimize(3 * cp.abs(edge_AD) + 6 * cp.abs(edge_AB) + 4 * cp.abs(edge_AC)
+                       + 5 * cp.abs(edge_DB) + 2 * cp.abs(edge_BC) + 7 * cp.abs(edge_BE))
+
+prob = cp.Problem(maximize, constraints)
+
+prob.solve()
+
+print("The optimal value is", prob.value)
+print("The value of edge_AD is", edge_AD.value)
+print("The value of edge_AB is", edge_AB.value)
+print("The value of edge_AC is", edge_AC.value)
+print("The value of edge_DB is", edge_DB.value)
+print("The value of edge_BC is", edge_BC.value)
+print("The value of edge_BE is", edge_BE.value)
+print("The value of edge_CE is", edge_CE.value)
---
+++ Assignment-2/triagent_social_welfare.py
@@ -0,0 +1,56 @@
+import cvxpy as cp
+
+# Define the variables
+p_01 = cp.Variable(1, nonneg=True)
+p_02 = cp.Variable(1, nonneg=True)
+p_03 = cp.Variable(1, nonneg=True)
+p_04 = cp.Variable(1, nonneg=True)
+
+p_11 = cp.Variable(1, nonneg=True)
+p_12 = cp.Variable(1, nonneg=True)
+p_13 = cp.Variable(1, nonneg=True)
+p_14 = cp.Variable(1, nonneg=True)
+
+p_21 = cp.Variable(1, nonneg=True)
+p_22 = cp.Variable(1, nonneg=True)
+p_23 = cp.Variable(1, nonneg=True)
+p_24 = cp.Variable(1, nonneg=True)
+
+# find a correlated equilibrium maximizing social welfare
+
+obj = cp.Maximize(3 * p_01 + p_03 + 6 * p_11 + 6 * p_14+p_23+3*p_24)
+
+constraints = [p_01 + p_02 + p_03 + p_04 + p_11 + p_12 + p_13 +
+               p_14 + p_21 + p_22 + p_23 + p_24 == 1]  # probabilities sum to 1
+
+# row player has no incentive to deviate
+constraints += [3*p_01 + 2*p_11 == p_03 + 2*p_14]
+
+
+# column player has no incentive to deviate
+
+constraints += [2*p_11+p_23 == 2 * p_04]
+
+# matrix player has no incentive to deviate
+constraints += [3*p_01 == 2*p_11+2*p_14]
+constraints += [p_01 == p_24]
+
+
+prob = cp.Problem(obj, constraints)
+
+prob.solve()
+
+
+print("The optimal value is", prob.value)
+print("The value of p_01 is", p_01.value)
+print("The value of p_02 is", p_02.value)
+print("The value of p_03 is", p_03.value)
+print("The value of p_04 is", p_04.value)
+print("The value of p_11 is", p_11.value)
+print("The value of p_12 is", p_12.value)
+print("The value of p_13 is", p_13.value)
+print("The value of p_14 is", p_14.value)
+print("The value of p_21 is", p_21.value)
+print("The value of p_22 is", p_22.value)
+print("The value of p_23 is", p_23.value)
+print("The value of p_24 is", p_24.value)
